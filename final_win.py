ad3b99569d9261302e15ee701045a634a7129c47
nonameGTWX <gabazun228@ukr.net>
2023-10-08 12:44:50 +0300

    okey

--- final_win.py
+++ final_win.py
@@ -1,33 +1,35 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, QGridLayout, 
+        QApplication, QWidget,
+        QHBoxLayout, QVBoxLayout, QGridLayout,
         QGroupBox, QRadioButton,
         QPushButton, QLabel, QListWidget, QLineEdit)
-        
+
 from instr import *
 
 class FinalWin(QWidget):
-    ''' вікно, в якому проводиться опитування '''
     def __init__(self):
-        
         super().__init__()
-
         self.initUI()
         self.set_appear()
         self.show()
-    
-    def initUI(self):
-       ''' створює графічні елементи '''
-       # TODO self.index_text = створити напис
-       # TODO self.result_text = створити напис
 
-        # TODO свтворити вертикальний лайоут і додати на нього створенні віджети
-       
+    def initUI(self):
+        index_text = QLabel("Index Label", self)
+        result_text = QLabel("Result Label", self)
+        btn = QPushButton("Knopka", self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(index_text)
+        vbox.addWidget(result_text)
+        vbox.addWidget(btn)
+
+        self.setLayout(vbox)
 
     def set_appear(self):
-        ''' встановлює, як виглядатиме вікно (напис, розмір, місце) '''
-        self.setWindowTitle(txt_title)
-        # TODO вствановити розмір вікна в pyqt
-        # TODO вствановити місце де вікно зявлятиметься в pyqt
+        self.setWindowTitle('Test Window')
+        self.setGeometry(0, 0, 240, 120)
 
+app = QApplication([])
+window = FinalWin()
+app.exec()
